32dac402d99347b2dc566667d089f42750416fa8
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2024-04-05 23:01:47 -0500

    Formateo de archivos

--- src/main/resources/static/response.js
+++ src/main/resources/static/response.js
@@ -1,60 +1,57 @@
 function enviar() {
-    let nombre = document.getElementById("nombre").value;
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        document.getElementById("arroba").innerHTML =
-            JSON.parse(this.responseText).arroba;
-    };
-    xhttp.open("GET", "/usuario/" + nombre);
-    xhttp.send();
+  let nombre = document.getElementById("nombre").value;
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("arroba").innerHTML = JSON.parse(this.responseText).arroba;
+  };
+  xhttp.open("GET", "/usuario/" + nombre);
+  xhttp.send();
 }
 
 function posts() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        document.getElementById("posts").innerHTML =
-        JSON.stringify(JSON.parse(this.responseText).posts);
-    };
-    xhttp.open("GET", "/hilo");
-    xhttp.send();
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function () {
+    document.getElementById("posts").innerHTML = JSON.stringify(JSON.parse(this.responseText).posts);
+  };
+  xhttp.open("GET", "/hilo");
+  xhttp.send();
 }
 
 function msg() {
-    let mensaje = document.getElementById("mensaje").value;
-    let arroba = document.getElementById("arroba").innerHTML;
+  let mensaje = document.getElementById("mensaje").value;
+  let arroba = document.getElementById("arroba").innerHTML;
 
-    let urlPost = "/post";
-    let urlHilo = "/hilo";
+  let urlPost = "/post";
+  let urlHilo = "/hilo";
 
-    let bodyData = {
-        'arroba': arroba,
-        'mensaje': mensaje
-    };
+  let bodyData = {
+    arroba: arroba,
+    mensaje: mensaje,
+  };
 
-    var formBody = [];
+  var formBody = [];
 
-    for (var property in bodyData) {
-        var encodedKey = encodeURIComponent(property);
-        var encodedValue = encodeURIComponent(bodyData[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-    }
+  for (var property in bodyData) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(bodyData[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
 
-    formBody = formBody.join("&");
+  formBody = formBody.join("&");
 
-    fetch(urlPost, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        },
-        body: formBody
-    });
+  fetch(urlPost, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: formBody,
+  });
 
-    fetch(urlHilo, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        },
-        body: formBody
-    })
-        .then(posts);
+  fetch(urlHilo, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: formBody,
+  }).then(posts);
 }
